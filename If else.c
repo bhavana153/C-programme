15863979e68876bc7c4b6a1a56dd45f0295be8ed
bhavana153 <sobhavana7@gmail.com>
2025-05-13 16:11:11 +0530

    Create If else.c

---
+++ If else.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+
+    
+    printf("Enter first number: ");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    printf("a is largest ");
+    else
+    
+    printf("b is largest");
+    return 0;
+}
