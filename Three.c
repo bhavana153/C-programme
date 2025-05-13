4e9fb0e91f8d9ae47927763a2bc7c006d4c58709
bhavana153 <sobhavana7@gmail.com>
2025-05-13 20:24:06 +0530

    Create Three.c

---
+++ Three.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, smallest;
+
+    
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    
+    if (a <= b && a <= c) {
+        smallest = a;
+    } else if (b <= a && b <= c) {
+        smallest = b;
+    } else {
+        smallest = c;
+    }
+
+   
+    printf("The smallest number is: %d\n", smallest);
+
+    return 0;
+}
