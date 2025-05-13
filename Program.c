22edd206474c2dc8622fceaefaf3e130362af3e5
bhavana153 <sobhavana7@gmail.com>
2025-05-13 13:01:42 +0530

    Program.c

---
+++ Program.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    
+    printf("Enter value of a: ");
+    scanf("%d", &a);
+
+    printf("Enter value of b: ");
+    scanf("%d", &b);
+
+   
+    printf("sum = %d\n", a + b);
+
+    return 0;
+}
