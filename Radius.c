4904868fc9f63c958c2ca30c184944dc88402f59
bhavana153 <sobhavana7@gmail.com>
2025-05-13 16:19:32 +0530

    Create Radius.c

---
+++ Radius.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float ac,r;
+    printf("enter radius:");
+    scanf("%f",&r);
+    ac=(3.142*r*r);
+    printf("Area of circle =%f",ac);
+    return 0;
+    
+}
