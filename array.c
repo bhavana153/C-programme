bb91f134ac882f97eb178d97804b6cb420e728f2
bhavana153 <sobhavana7@gmail.com>
2025-05-14 12:23:14 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int row,col;
+int bhav[5]={1,2,3,4,5};
+int main()
+{
+    
+    for(row=0;row=6;row++)
+{
+    printf("%d\n",bhav[row]);
+}
+return 0;
+}
+    
+
+                        
