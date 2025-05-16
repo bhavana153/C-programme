8c418e146f238ba12069ea165923ef4988b08ea8
bhavana153 <sobhavana7@gmail.com>
2025-05-16 15:18:26 +0530

    Create structure.c

---
+++ structure.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    struct bavana{
+        char name[5];
+        int age;
+        float weight;
+    };
+    struct bavana r1;
+    printf("enter the name");
+    scanf("%s",&r1);
+    printf("enter the age");
+    scanf("%d",&r1);
+    printf("enter the weight");
+    scanf("%f",&r1);
+    }
